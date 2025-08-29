1ce0c6d96b878f28b4f88a3e682d2f297b5b707f
FikryEfendi <fikry.efendi2336@gmail.com>
2025-08-29 18:35:22 +0700

    Biodata Muhammad Fikry Efendi

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hallo dunia!");
+        String nama = "Muhammad Fikry Efendi";
+        final long NIM = 2407112294L;
+        double[] ip = {3.86, 3.77};
+        String alamat = "Jalan Naga Sakti Perum Griya Kenari Indah Blok H76";
+        int umur = 19;
+        char golonganDarah = 'B';
+        byte ipByte = (byte) ip[0];
+
+        System.out.println("Halo... Nama Saya "+ nama);
+        System.out.println("NIM saya yaitu "+ NIM);
+        System.out.println("Jika kamu ingin mengunjungi saya, bisa langsung kerumah saya di "+ alamat);
+        System.out.println("Saat ini saya berusia "+ umur);
+        System.out.println("Golongan darah saya yaitu "+ golonganDarah);
+        System.out.println("IP Semester 1 saya "+ ip[0]);
+        System.out.println("IP Semester 2 saya "+ ip[1]);
+        System.out.println("IP Semester 1 dalam Byte "+ ipByte);
     }
 }
