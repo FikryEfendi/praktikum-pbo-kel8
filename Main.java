16dd884dc5ef66b978f379b867fe3d63d2dcc629
Aflahsholeh <aflahsholeh3@gmail.com>
2025-08-30 16:34:42 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,12 +11,12 @@ public class Main
     char golongan_darah = 'A';
     byte ipByte = 3;
 
-    System.out.println("nama saya " + nama);
-    System.out.println("Nim saya adalah " +Nim);
-    System.out.println("Ip saya di Semester 1 dan 2 adalah " +ip[0]+" "+ip[1]);
-    System.out.println("saat ini tinggal di " +alamat);
-    System.out.println("umur saya saat ini  "+umur);
-    System.out.println("golongan darah saya adalah " +golongan_darah);
+    System.out.println("nama saya adalah " + nama);//Nama saya
+    System.out.println("Nim saya adalah " +Nim);///NIm saya
+    System.out.println("Ip saya di Semester 1 dan 2 adalah " +ip[0]+" "+ip[1]);//IPK saya
+    System.out.println("saat ini tinggal di " +alamat);//Alamat saya
+    System.out.println("umur saya saat ini  "+umur);//Umur saya
+    System.out.println("golongan darah saya adalah " +golongan_darah);//GOlongan darah saya
     System.out.println("ip Semester 2 dalam byte " + ipByte);
 
   }
