7999f7daed2c62d606b8b499f9bfd93e42859934
nabilfathan <akunmiliksaya10@gmail.com>
2025-09-20 23:24:10 +0700

    Update web

--- Klubbesar.java
+++ Klubbesar.java
@@ -6,13 +6,13 @@ public class Klubbesar
         System.out.println("-------------------------------------------DATA KLUB SEPAKBOLA BESAR DI DUNIA------------------------------------------");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------");
         Klubbola klubbola1 = new Klubbola("Real Madrid","Madrid", 130,1902 );
-        klubbola1.tampilkanTim("UNO DOS TRES HALA MADRID!!!");
-       
-        Klubbola klubbola2 = new Klubbola("Barcelona","Barcelona", 1899 ); // tanpa pake trophy
-        klubbola2.tampilkanTim();
-      
+        Klubbola klubbola2 = new Klubbola("Barcelona","Barcelona", 1899 ); // gk make trophy
         Klubbola klubbola3 = new Klubbola("Manchester United","Manchester", 66,1878 );
+       
+        klubbola1.tampilkanTim("UNO DOS TRES HALA MADRID!!!");
+        klubbola2.tampilkanTim();
         klubbola3.tampilkanTim();
+      
 
     } 
 }
--- Klubbola.java
+++ Klubbola.java
@@ -1,42 +1,42 @@
 public class Klubbola {
-    String nama;
+    String namaTim;
     String kota;
     int trophy;
     int tahunBerdiri;
 
     // Konstruktor
-    public Klubbola(String nama, String kota, int trophy, int tahunBerdiri) {
-        this.nama = nama;
+    public Klubbola(String namaTim, String kota, int trophy, int tahunBerdiri) {
+        this.namaTim = namaTim;
         this.kota = kota;
         this.trophy = trophy;
         this.tahunBerdiri = tahunBerdiri;
     }
 
-    public Klubbola(String nama, String kota, int tahunBerdiri){
-        this.nama = nama;
+    public Klubbola(String namaTim, String kota, int tahunBerdiri){
+        this.namaTim = namaTim;
         this.kota = kota;
         this.tahunBerdiri = tahunBerdiri;
         this.trophy = 0;
     }
 
     public void tampilkanTim(String pesan) {
-        System.out.println("Klub Sepakbola " + nama + " berasal dari kota " + kota + " dan berdiri pada tahun " + tahunBerdiri + " dan kini memiliki " + trophy + " trophy");
+        System.out.println("Klub Sepakbola " + namaTim + " berasal dari kota " + kota + " dan berdiri pada tahun " + tahunBerdiri + " dan kini memiliki " + trophy + " trophy");
         System.out.println(pesan);
         System.out.println("-----------------------------------------------------------------------------------------------------------------------");
     }
 
     public void tampilkanTim() {
-        System.out.println("Klub Sepakbola " + nama + " berasal dari kota " + kota + " dan berdiri pada tahun " + tahunBerdiri + " dan kini memiliki " + trophy + " trophy");
+        System.out.println("Klub Sepakbola " + namaTim + " berasal dari kota " + kota + " dan berdiri pada tahun " + tahunBerdiri + " dan kini memiliki " + trophy + " trophy");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------");
     }
 
     /* Getter dan Setter
     public String getNama() {
-        return nama;
+        return namaTim;
     }
 
-    public void setNama(String nama) {
-        this.nama = nama;
+    public void setNama(String namaTim) {
+        this.namaTim = namaTim;
     }
 
     public String getKota() {
@@ -58,7 +58,7 @@ public class Klubbola {
     @Override
     public String toString() {
         return "Klubbola{" +
-                "nama='" + nama + '\'' +
+                "namaTim='" + namaTim + '\'' +
                 ", kota='" + kota + '\'' +
                 ", tahunBerdiri=" + tahunBerdiri +
                 '}';
