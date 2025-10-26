38e17a1fcb66f9e20dc61f8cb81b572bdd6d8860
hafizahdinia <hafizahdiniaqila@gmail.com>
2025-10-26 10:26:16 +0700

    Latihan praktikum bab 8

---
+++ ProjectBab8Latihan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.praktikumpbo.projectbab8latihan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProjectBab8Latihan {
+
+    public static void main(String[] args) {
+        new Kalkulator().setVisible(true);
+    }
+}
