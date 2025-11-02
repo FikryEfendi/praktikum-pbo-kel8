047c526fb78f2d968d4278c0d2a6bebc5ae9cda0
FikryEfendi <fikry.efendi2336@gmail.com>
2025-11-03 02:13:19 +0700

    Praktikum 8 Hasil Praktikum

---
+++ ProjectBab8Hasil2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.praktikumpbo.projectbab8hasil2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProjectBab8Hasil2 {
+
+    public static void main(String[] args) {
+        new LoginJFrame().setVisible(true);
+    }
+}
